4257311d1399bb03a43a2210a2d9a9f510cec90c
Adhitya Kusumadinatha <adhityasantika@yahoo.com>
2020-06-12 16:28:23 +0800

    Done Register Screen, App Icon, and text field component.

--- app/src/main/java/com/gelora/mitra/activity/WelcomeScreenActivity.java
+++ app/src/main/java/com/gelora/mitra/activity/WelcomeScreenActivity.java
@@ -2,15 +2,28 @@ package com.gelora.mitra.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.gelora.mitra.R;
 
 public class WelcomeScreenActivity extends AppCompatActivity {
-
+Button daftar, masuk;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_screen);
+        daftar = findViewById(R.id.daftarButton);
+        masuk = findViewById(R.id.masukButton);
+
+        daftar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(WelcomeScreenActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
