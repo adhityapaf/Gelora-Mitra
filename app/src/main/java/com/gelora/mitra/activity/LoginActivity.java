23f44ea42b28e753fdbc2aceca6382d9beae1818
Adhitya Kusumadinatha <adhityasantika@yahoo.com>
2020-06-13 21:52:31 +0800

    Done navigation bar

--- app/src/main/java/com/gelora/mitra/activity/LoginActivity.java
+++ app/src/main/java/com/gelora/mitra/activity/LoginActivity.java
@@ -2,15 +2,27 @@ package com.gelora.mitra.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.gelora.mitra.R;
 
 public class LoginActivity extends AppCompatActivity {
-
+    Button masuk;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        masuk = findViewById(R.id.loginSubmitButton);
+        masuk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/gelora/mitra/activity/MainActivity.java
+++ app/src/main/java/com/gelora/mitra/activity/MainActivity.java
@@ -1,10 +1,18 @@
 package com.gelora.mitra.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.gelora.mitra.R;
+import com.gelora.mitra.fragments.AkunFragment;
+import com.gelora.mitra.fragments.DashboardFragment;
+import com.gelora.mitra.fragments.LapanganFragment;
+import com.gelora.mitra.fragments.PesananFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +20,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment = null;
+
+            switch (item.getItemId()){
+                case R.id.nav_dashboard:
+                    selectedFragment = new DashboardFragment();
+                    break;
+                case R.id.nav_lapangan:
+                    selectedFragment = new LapanganFragment();
+                    break;
+                case R.id.nav_pesanan:
+                    selectedFragment = new PesananFragment();
+                    break;
+                case R.id.nav_akun:
+                    selectedFragment = new AkunFragment();
+                    break;
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+            return true;
+        }
+    };
 }
