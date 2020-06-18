c1540f9d9fe041838d8f852b7fbf4b714a1bb69a
Adhitya Kusumadinatha <adhityasantika@yahoo.com>
2020-06-18 14:08:01 +0800

    Done TambahkanLapangan layout

--- app/src/main/java/com/gelora/mitra/fragments/LapanganFragment.java
+++ app/src/main/java/com/gelora/mitra/fragments/LapanganFragment.java
@@ -1,5 +1,6 @@
 package com.gelora.mitra.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,13 +14,24 @@ import androidx.recyclerview.widget.ListAdapter;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.gelora.mitra.R;
+import com.gelora.mitra.activity.TambahkanLapangan;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class LapanganFragment extends Fragment {
+    FloatingActionButton floatingActionButton;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_lapangan, container, false);
         RecyclerView recyclerView = view.findViewById(R.id.lapangan_recycler);
+        floatingActionButton = view.findViewById(R.id.tambahLapangan_fab);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), TambahkanLapangan.class);
+                startActivity(intent);
+            }
+        });
 //        ListAdapter listAdapter = new ListAdapter();
 //        recyclerView.setAdapter(listAdapter);
 //        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
