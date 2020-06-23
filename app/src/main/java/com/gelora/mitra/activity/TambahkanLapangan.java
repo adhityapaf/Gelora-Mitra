97906e76970e0d89465e33dd4568fd34e4735b02
Adhitya Kusumadinatha <adhityasantika@yahoo.com>
2020-06-23 12:42:39 +0800

    Done TimePicker Fragment

--- app/src/main/java/com/gelora/mitra/activity/TambahkanLapangan.java
+++ app/src/main/java/com/gelora/mitra/activity/TambahkanLapangan.java
@@ -1,16 +1,55 @@
 package com.gelora.mitra.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 import com.gelora.mitra.R;
+import com.gelora.mitra.fragments.TimePickerFragment;
 
-public class TambahkanLapangan extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class TambahkanLapangan extends AppCompatActivity implements TimePickerDialog.OnTimeSetListener {
+    ImageView backButton;
+    Button pilihJamButton;
+    ArrayList<String> stringArrayList = new ArrayList<String>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tambahkan_lapangan);
+        backButton = findViewById(R.id.back_arrow);
+        pilihJamButton = findViewById(R.id.pilihJam_button);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+        pilihJamButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment timePicker = new TimePickerFragment();
+                timePicker.show(getSupportFragmentManager(), "time picker");
+            }
+        });
+    }
+
+    // Mengambil data dari TimePicker
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        TextView jampilihan;
+        jampilihan = findViewById(R.id.jamPilihanText);
+        jampilihan.setText("Jam : "+hourOfDay+" Menit : "+minute);
+        stringArrayList.add(jampilihan.getText().toString());
+        Log.d("TimePicker", "onTimeSet: "+stringArrayList.size()+"Isi : "+stringArrayList.get(stringArrayList.size()-1));
+
     }
 }
--- app/src/main/java/com/gelora/mitra/model/LapanganData.java
+++ app/src/main/java/com/gelora/mitra/model/LapanganData.java
@@ -4,33 +4,5 @@ import com.gelora.mitra.R;
 
 public class LapanganData {
 
-    public static int[] lapangan_image = new int[]{
-            R.drawable.background_fragments,
-            R.drawable.background_fragments
-    };
 
-    public static String[] lapangan_title = new String[]{
-            "Lapangan Gelora",
-            "Boss Futsal"
-    };
-
-    public static String[] lapangan_type = new String[]{
-            "Basket",
-            "Futsal"
-    };
-
-    public static String[] rumput_lapangan = new String[]{
-            "Vinyl",
-            "Rumput"
-    };
-
-    public static String[] lapangan_time = new String[]{
-            "17.00",
-            "19.00"
-    };
-
-    public static int[] lapangan_price = new int[]{
-            100000,
-            150000
-    };
 }
