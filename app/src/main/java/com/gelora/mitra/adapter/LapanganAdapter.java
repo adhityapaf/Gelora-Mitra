cbd1996d2f1dcd5c56937e5b74849d84be4ee2f0
Adhitya Kusumadinatha <adhityasantika@yahoo.com>
2020-07-03 16:33:58 +0800

    Done : Hapus Lapangan logic

--- app/src/main/java/com/gelora/mitra/adapter/LapanganAdapter.java
+++ app/src/main/java/com/gelora/mitra/adapter/LapanganAdapter.java
@@ -1,13 +1,17 @@
 package com.gelora.mitra.adapter;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.DividerItemDecoration;
@@ -18,7 +22,16 @@ import com.bumptech.glide.Glide;
 import com.gelora.mitra.R;
 import com.gelora.mitra.activity.EditLapanganActivity;
 import com.gelora.mitra.model.LapanganData;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -28,9 +41,11 @@ public class LapanganAdapter extends RecyclerView.Adapter<LapanganAdapter.ViewHo
 
     ArrayList<LapanganData> lapanganData;
     Context mContext;
-    DatabaseReference ref;
+    DatabaseReference lapanganRef, pemilikRef, totalLapanganCounter;
     HashMap<String, String> hashMap = new HashMap<String,String>();
     ArrayList<String> jamSewaArrayList;
+    int pemilikCounter = 0;
+    private static final String TAG = "LapanganAdapter";
     public static final String ID_LAPANGAN = "com.gelora.mitra.id_lapangan";
     public static final String NAMA_LAPANGAN = "com.gelora.mitra.nama_lapangan";
     public static final String KATEGORI_LAPANGAN = "com.gelora.mitra.kategori_lapangan";
@@ -48,6 +63,20 @@ public class LapanganAdapter extends RecyclerView.Adapter<LapanganAdapter.ViewHo
     @Override
     public LapanganAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(mContext).inflate(R.layout.card_holder_lapangan, parent, false);
+        lapanganRef = FirebaseDatabase.getInstance().getReference("lapangan");
+        pemilikRef = FirebaseDatabase.getInstance().getReference("pemilik_lapangan").child(FirebaseAuth.getInstance().getCurrentUser().getUid());
+        totalLapanganCounter = pemilikRef.child("total_lapangan");
+        totalLapanganCounter.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                pemilikCounter = Integer.parseInt(snapshot.getValue().toString());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
         return new ViewHolder(view);
     }
 
@@ -92,6 +121,47 @@ public class LapanganAdapter extends RecyclerView.Adapter<LapanganAdapter.ViewHo
                 intent.putExtra(HARGA_LAPANGAN, hargaLapanganStr);
                 intent.putExtra(GAMBAR_LAPANGAN, gambarLapanganStr);
                 mContext.startActivity(intent);
+            }
+        });
+        holder.hapusLapangan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final AlertDialog alertDialog = new AlertDialog.Builder(mContext).create();
+                final AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+                builder.setTitle("Hapus Lapangan");
+                builder.setMessage("Apakah Anda yakin menghapus lapangan " + holder.namaLapangan.getText() + " ?");
+                builder.setNegativeButton("Tidak", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        alertDialog.cancel();
+                    }
+                });
+                builder.setPositiveButton("Hapus", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String idLapangan = lapanganData.get(position).getId_lapangan();
+                        FirebaseStorage photoStorage = FirebaseStorage.getInstance().getReference("foto_lapangan/"+FirebaseAuth.getInstance().getCurrentUser().getUid()+"/id_lapangan/"+idLapangan).getStorage();
+                        StorageReference photoRef =  photoStorage.getReferenceFromUrl(lapanganData.get(position).getGambar_lapangan());
+                        pemilikCounter--;
+                        lapanganRef.child("id_lapangan").child(idLapangan).removeValue();
+                        pemilikRef.child("id_lapangan").child(idLapangan).removeValue();
+                        totalLapanganCounter.setValue(pemilikCounter);
+                        photoRef.delete().addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Log.d(TAG, "onSuccess: Foto lapangan dihapus");
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.d(TAG, "onFailure: Foto lapangan gagal dihapus!");
+                            }
+                        });
+                        Toast.makeText(mContext, "Lapangan "+holder.namaLapangan.getText()+" berhasil dihapus!", Toast.LENGTH_SHORT).show();
+                        notifyItemRemoved(position);
+                    }
+                });
+                builder.show();
             }
         });
     }
